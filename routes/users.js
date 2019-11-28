6038389cdc97d1cf5b991a0bd92657df1e757d52
maro07 <roberto.motamarinho@fhsstudents.ch>
2019-11-28 08:51:11 +0100

    test

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,23 @@
-var express = require('express');
+var express = require('express'); 
 var router = express.Router();
+
 var chatHistory = [];
-var nickname = [];
+var nicknames = [];
 
-/* GET users listing. 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) { 
+  res.json({ message: 'fhs chat-app api works...' }); 
 });
-*/
 
-router.get('/', function (req, res, next) {
-  res.json({message: 'fhs chat-app api works...'});
+router.get('/history', function (req, res, next) { 
+  res.send(chatHistory); 
+});
+
+router.post('/history', function (req, res, next) {
+  var date = new Date();
+
+  chatHistory.push({ message: req.body.message, nickname: req.body.nickname, date: date });
+  
+  res.json({ message: 'History created!' });
 });
 
 module.exports = router;
