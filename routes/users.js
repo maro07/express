116ac919b80d4021bf6c137ab030d778bbffd2a6
maro07 <roberto.motamarinho@fhsstudents.ch>
2019-12-02 16:30:30 +0100

    asdf

--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,27 @@ var router = express.Router();
 
 var chatHistory = [];
 var nicknames = [];
+var newmessages = false;
 
-router.get('/', function (req, res, next) {
+/*router.get('/', function (req, res, next) {
   res.json({ message: 'fhs chat-app api works...' });
-});
+});*/
 
 
 router.get('/history', function (req, res, next) {
   res.send(chatHistory);
 });
 
+router.get('/', function (req, res, next) {
+  res.json({ change: 'asdf' });
+});
+
 
 router.post('/history', function (req, res, next) {
   var date = new Date();
-
   chatHistory.push({ nickname: req.body.username, message: req.body.content, date: req.body.timestamp });
-
   res.json({ message: 'History created!' });
+  newmessages = true;
 });
 
 module.exports = router;
