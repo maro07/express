c0fdb1cbfa2249c87b6e9b6e8854e90ab5b4427d
maro07 <roberto.motamarinho@fhsstudents.ch>
2019-12-12 09:33:24 +0100

    changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var allowedOrigins=['http://localhost:4200', 'http://blacknwhitechat.herokuapp.com/', 'https://blacknwhitechat.herokuapp.com/'];
+var allowedOrigins=['http://localhost:4200', 'http://blacknwhitechat.herokuapp.com', 'https://blacknwhitechat.herokuapp.com'];
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
